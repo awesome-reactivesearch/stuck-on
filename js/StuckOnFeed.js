4f564321936653e942ba8e143c14610f02e45974
Yash Shah <mail@yashshah.com>
2016-02-05 23:01:35 +0530

    Refactor code

--- js/StuckOnFeed.js
+++ js/StuckOnFeed.js
@@ -1,42 +1,42 @@
 // Render a single item of the Feed
 var FeedItem = React.createClass({
-	render: function() {
-		// Get the profile picture from Twitter using the given handle
-		twitterProfilePictureURL = "https://twitter.com/" + this.props.item.twitterHandle + "/profile_image?size=original"
-		return (
-			<div className="row">
-				<div className="col s4 m2 l1">
-		        	<img className="profile-picture" src={twitterProfilePictureURL}/>
-		        </div>
-				<div className="col s8 m10 l11">
-		          	<span className="twitter-handle">{this.props.item.twitterHandle} is</span>
-		          	<p className="stuck-on-feed">{this.props.item.status}</p>
-		        </div>
-	        </div>
-		);
-	}
+    render: function() {
+        // Get the profile picture from Twitter using the given handle
+        let twitterProfilePictureURL = "https://twitter.com/" + this.props.item.twitterHandle + "/profile_image?size=original"
+        return (
+            <div className="row">
+                <div className="col s4 m2 l1">
+                    <img className="profile-picture" src={twitterProfilePictureURL}/>
+                </div>
+                <div className="col s8 m10 l11">
+                    <span className="twitter-handle">{this.props.item.twitterHandle} is</span>
+                    <p className="stuck-on-feed">{this.props.item.status}</p>
+                </div>
+            </div>
+        );
+    }
 })
 
 // Collection of the items of the feed
 var FeedContainer = React.createClass({
-	render: function() {
-		var content;
-		// Loop through all the items
-		if (this.props.items.length > 0) {
-		  content = this.props.items.map(function(item) {
-		    return <FeedItem item={item} />;
-		  });
-		} else {
-		  content = <FeedItem item="No content Available!" />;
-		}
+    render: function() {
+        var content
+            // Loop through all the items
+        if (this.props.items.length > 0) {
+            content = this.props.items.map(function(item) {
+                return <FeedItem item={item} />
+            });
+        } else {
+            content = <FeedItem item="No content Available!" />
+        }
 
-		return (
-			<div className="row">
-				<h3 className="col s12 center white-text"> Stuck On Feed</h3>
-				{content}
-			</div>
-		);
-	}
+        return (
+            <div className="row">
+                <h3 className="col s12 center white-text"> Stuck On Feed</h3>
+                {content}
+            </div>
+        );
+    }
 })
 
 // Fetch the data from Appbase and renders FeedContainer
@@ -44,114 +44,115 @@ var FeedContainer = React.createClass({
 // Listen to Realtime updates
 var StuckOnFeed = React.createClass({
 
-	getDefaultProps: function() {
-		return {
-			pageNumber: 0
-		};
-	},
-	getInitialState: function() {
-		var self = this;
+    getInitialState: function() {
+        var self = this;
 
-		// Get the Appbase credentials from the config file
-		// Note that this will be executed as async process
-		$.getJSON("./config.json", function(json) {
-			// Create Appbase reference object
-			self.props.appbaseRef = new Appbase({
-				  url: 'https://scalr.api.appbase.io',
-				  appname: json.appbase.app_name,
-				  username: json.appbase.username,
-				  password: json.appbase.password
-			});
-			self.props.type = json.appbase.type	
-			self.getHistoricalFeed()
-			self.subscribeToUpdates()	
-		});
+        // Get the Appbase credentials from the config file
+        // Note that this will be executed as async process
+        $.getJSON("./config.json", function(json) {
+            // Create Appbase reference object
+            self.appbaseRef = new Appbase({
+                url: 'https://scalr.api.appbase.io',
+                appname: json.appbase.app_name,
+                username: json.appbase.username,
+                password: json.appbase.password
+            });
+            self.type = json.appbase.type
+            self.pageNumber = 0
+            self.getHistoricalFeed()
+            self.subscribeToUpdates()
+        });
 
-		return {
-			items: [] 
-		};
-	},
+        return {
+            items: []
+        };
+    },
 
-	// Fetch the old status from Appbase based on pageNumber
-	getHistoricalFeed: function(){
-		self = this
-		self.props.appbaseRef.search({
-			type: self.props.type,
-		  	size: 10,
-		  	from: self.props.pageNumber*10,
-			body: {
-			    query: {
-			      match_all: {}
-			    },
-			    sort: {
-			    	timestamp: "desc"
-			    }
-			  }
-			}).on('data', function(res) {
-				self.props.pageNumber = self.props.pageNumber + 1
-				self.addItemsToFeed(res.hits.hits)
-			}).on('error', function(err) {
-			  console.log("search error: ", err);
-		})
-	},
+    // Fetch the old status from Appbase based on pageNumber
+    getHistoricalFeed: function() {
+        self = this
+        self.appbaseRef.search({
+            type: self.type,
+            size: 10,
+            from: self.pageNumber * 10,
+            body: {
+                query: {
+                    match_all: {}
+                },
+                sort: {
+                    timestamp: "desc"
+                }
+            }
+        }).on('data', function(res) {
+            console.log(self.pageNumber = self.pageNumber + 1)
+            self.addItemsToFeed(res.hits.hits)
+        }).on('error', function(err) {
+            console.log("search error: ", err);
+        })
+    },
 
-	// Add the items to the feed fetched in getHistorialFeed
-	addItemsToFeed: function(newItems){
-		var updated = this.state.items;
-		$.map(newItems, function(object){
-			updated.push(object._source)
-		})
-		this.setState({items: updated});
-	},	
+    // Add the items to the feed fetched in getHistorialFeed
+    addItemsToFeed: function(newItems) {
+        console.log(newItems)
+        var updated = this.state.items;
+        $.map(newItems, function(object) {
+            updated.push(object._source)
+        })
+        this.setState({
+            items: updated
+        });
+    },
 
-	// For the Infinite scrolling
-	componentDidMount: function() {
-		window.addEventListener('scroll', this.handleScroll)
-	},
-	componentWillUnmount: function() {
-		window.removeEventListener('scroll', this.handleScroll)
-	},
+    // For the Infinite scrolling
+    componentDidMount: function() {
+        window.addEventListener('scroll', this.handleScroll)
+    },
+    componentWillUnmount: function() {
+        window.removeEventListener('scroll', this.handleScroll)
+    },
 
-	// Fired when client scrolls the page
-	handleScroll: function(event) {
-		var body = event.srcElement.body
-		// When the client reaches at the bottom of the page, get next page		
-		if(body.clientHeight + body.scrollTop >= body.offsetHeight) {
-			this.getHistoricalFeed()
-		}
-	},
-	// Listen to realtime updates in Appbase and then add it to the top of the feed
-	subscribeToUpdates: function(){
-		self.props.appbaseRef.searchStream({
-		  type: "feed",
-		  body: {
-		    query: {
-		      match_all: {}
-		    }
-		  }
-		}).on('data', function(res) {
-		  self.addItemToTop(res._source)
-		}).on('error', function(err) {
-		  console.log("streaming error: ", err);
-		})
-	},
+    // Fired when client scrolls the page
+    handleScroll: function(event) {
+        var body = event.srcElement.body
+            // When the client reaches at the bottom of the page, get next page     
+        if (body.clientHeight + body.scrollTop >= body.offsetHeight) {
+            this.getHistoricalFeed()
+        }
+    },
+    // Listen to realtime updates in Appbase and then add it to the top of the feed
+    subscribeToUpdates: function() {
+        self.appbaseRef.searchStream({
+            type: "feed",
+            body: {
+                query: {
+                    match_all: {}
+                }
+            }
+        }).on('data', function(res) {
+            self.addItemToTop(res._source)
+        }).on('error', function(err) {
+            console.log("streaming error: ", err);
+        })
+    },
 
-	// Add the realtime status to the top
-	addItemToTop: function(newItem){
-		var updated = this.state.items;
-		updated.unshift(newItem)
-		this.setState({items: updated});
-	},
+    // Add the realtime status to the top
+    addItemToTop: function(newItem) {
+        var updated = this.state.items;
+        updated.unshift(newItem)
+        this.setState({
+            items: updated
+        });
+    },
 
-	render: function() {
-		return (
-			<FeedContainer items={this.state.items}/>
-		);
-	}
+    render: function() {
+        return (
+            <FeedContainer items={this.state.items}/>
+        );
+    }
 
 });
 
-React.render(
-  <StuckOnFeed />,
-  document.getElementById('stuckOnFeed')
+ReactDOM.render(
+    <StuckOnFeed />,
+    document.getElementById('stuckOnFeed')
 );
--- js/StuckOnInput.js
+++ js/StuckOnInput.js
@@ -1,141 +1,141 @@
 // Component for input of Twitter handle
 var TwitterInput = React.createClass({
-	componentDidMount: function() {
-		// Focus on the input button when the page loads
-		this.refs.nameInput.getDOMNode().focus(); 
-	},
-	_handleKeyPress: function(e) {
-		// Call the onSubmit event when enter is pressed
-		if (e.key === 'Enter') {
-			if(this.refs.nameInput.getDOMNode().value){
-				this.props.onSubmit(this.refs.nameInput.getDOMNode().value);
-			}
-		}
-	},
-	render: function () {
-		return (
-			<div className="statusInput">
-				<h2>What is your Twitter username?</h2>
-				<input type = "text" onKeyPress = {this._handleKeyPress} placeholder="@YourTwitterUsername" ref="nameInput" />
-			</div>
-		)
-	}
+    componentDidMount: function() {
+        // Focus on the input button when the page loads
+        this.refs.nameInput.focus();
+    },
+    _handleKeyPress: function(e) {
+        // Call the onSubmit event when enter is pressed
+        if (e.key === 'Enter') {
+            if (this.refs.nameInput.value) {
+                this.props.onSubmit(this.refs.nameInput.value);
+            }
+        }
+    },
+    render: function() {
+        return (
+            <div className="statusInput">
+                <h2>What is your Twitter username?</h2>
+                <input type = "text" onKeyPress = {this._handleKeyPress} placeholder="@YourTwitterUsername" ref="nameInput" />
+            </div>
+        )
+    }
 });
 
 // Component for input of Status
 var StatusInput = React.createClass({
-	// Focus on the input button when the page loads
-	componentDidMount: function() {
-		this.refs.statusInput.getDOMNode().focus(); 
-	},
-	_handleKeyPress: function(e) {
-		// Call the onSubmit event when enter is pressed
-		if (e.key === 'Enter') {
-			if(this.refs.nameInput.getDOMNode().value){
-				this.props.onSubmit(this.refs.statusInput.getDOMNode().value);
-				this.refs.statusInput.getDOMNode().value = "";
-			}
-		}
-	},
-	render: function () {
-		return (
-			<div className="statusInput">
-				<h2>I am Stuck on </h2>
-				<input type = "text" onKeyPress = {this._handleKeyPress} placeholder={this.props.placeholder} ref="statusInput" />
-			</div>
-		)
-	}
+    // Focus on the input button when the page loads
+    componentDidMount: function() {
+        this.refs.statusInput.focus();
+    },
+    _handleKeyPress: function(e) {
+        // Call the onSubmit event when enter is pressed
+        if (e.key === 'Enter') {
+            if (this.refs.statusInput.value) {
+                this.props.onSubmit(this.refs.statusInput.value);
+                this.refs.statusInput.value = "";
+            }
+        }
+    },
+    render: function() {
+        return (
+            <div className="statusInput">
+                <h2>I am Stuck on </h2>
+                <input type = "text" onKeyPress = {this._handleKeyPress} placeholder={this.props.placeholder} ref="statusInput" />
+            </div>
+        )
+    }
 });
 
 var StuckOnInput = React.createClass({
 
-	// Checks if localstorage already exists otherwise update it with default value
-	getInitialState: function(){
-		var self = this
+    // Checks if localstorage already exists otherwise update it with default value
+    getInitialState: function() {
+        var self = this
+        let status
+        let twitterHandle
 
-		// Get the Appbase credentials from the config file
-		// Note that this will be executed as async process
-		$.getJSON("./config.json", function(json) {
-			// Create Appbase reference object
-			self.props.appbaseRef = new Appbase({
-				  url: 'https://scalr.api.appbase.io',
-				  appname: json.appbase.app_name,
-				  username: json.appbase.username,
-				  password: json.appbase.password
-			});
-			self.props.type = json.appbase.type
-		});
+        // Get the Appbase credentials from the config file
+        // Note that this will be executed as async process
+        $.getJSON("./config.json", function(json) {
+            // Create Appbase reference object
+            self.appbaseRef = new Appbase({
+                url: 'https://scalr.api.appbase.io',
+                appname: json.appbase.app_name,
+                username: json.appbase.username,
+                password: json.appbase.password
+            });
+            self.type = json.appbase.type
+        });
 
-		// If there is twitter handle in localstorage, get it and use that as default
-		// Also, get the status from the localstorage to show as the placeholder
-		if (localStorage.state){
-			status = JSON.parse(localStorage.state).status
-			twitterHandle = JSON.parse(localStorage.state).twitterHandle
-		}
-		// Setting the status with default Value
-		else
-		{
-			status = "Mining Bitcoin"
-			twitterHandle = ""
-		}
-		return {
-			status: status,
-			twitterHandle: twitterHandle
-		};
-	},
+        // If there is twitter handle in localstorage, get it and use that as default
+        // Also, get the status from the localstorage to show as the placeholder
+        if (localStorage.state) {
+            status = JSON.parse(localStorage.state).status
+            twitterHandle = JSON.parse(localStorage.state).twitterHandle
+        }
+        // Setting the status with default Value
+        else {
+            status = "Mining Bitcoin"
+            twitterHandle = ""
+        }
+        return {
+            status: status,
+            twitterHandle: twitterHandle
+        };
+    },
 
-	// Update the localStorage when state is changed
-	componentDidUpdate: function(prevProps, prevState) {
-		localStorage.state = JSON.stringify(this.state);
-	},
+    // Update the localStorage when state is changed
+    componentDidUpdate: function(prevProps, prevState) {
+        localStorage.state = JSON.stringify(this.state);
+    },
 
-	// Index the status to Appbase
-	addInputToAppbase: function(status){
-		// Set the status state with the argument passed with the function
-		this.setState({
-			status:status 
-		});
-		var data = {
-			"status": status,
-			"twitterHandle": this.state.twitterHandle,
-			"timestamp": Date.now()
-		}
-		this.props.appbaseRef.index({
-		    type: this.props.type,
-		    body: data,
-		}).on('data', function(response) {
-		    console.log(response);
-		}).on('error', function(error) {
-		    console.log(error);
-		});
-	},
+    // Index the status to Appbase
+    addInputToAppbase: function(status) {
+        // Set the status state with the argument passed with the function
+        this.setState({
+            status: status
+        });
+        var data = {
+            "status": status,
+            "twitterHandle": this.state.twitterHandle,
+            "timestamp": Date.now()
+        }
+        this.appbaseRef.index({
+            type: this.type,
+            body: data,
+        }).on('data', function(response) {
+            console.log(response);
+        }).on('error', function(error) {
+            console.log(error);
+        });
+    },
 
-  	// Update the Twitter Handle state when the TwitterInput component is submitted
-	setTwitterHandle: function(twitterHandle){
-		this.setState({
-			twitterHandle: twitterHandle 
-		});
-	},
+    // Update the Twitter Handle state when the TwitterInput component is submitted
+    setTwitterHandle: function(twitterHandle) {
+        this.setState({
+            twitterHandle: twitterHandle
+        });
+    },
 
-	render: function() {
-		var twitterHandle = ""
-		
-		// If twitterHandle is already set in localStorage, then show update status page directly
-		if(this.state.twitterHandle){
-			return (
-				<StatusInput onSubmit={this.addInputToAppbase} placeholder={this.state.status}/>
-			);					
-		}
-		else{
-			return (
-				<TwitterInput onSubmit={this.setTwitterHandle} />
-			);
-		}
-	}
+    render: function() {
+        var twitterHandle = ""
 
-  });
+        // If twitterHandle is already set in localStorage, then show update status page directly
+        if (this.state.twitterHandle) {
+            return (
+                <StatusInput onSubmit={this.addInputToAppbase} placeholder={this.state.status}/>
+            );
+        } else {
+            return (
+                <TwitterInput onSubmit={this.setTwitterHandle} />
+            );
+        }
+    }
 
-React.render(
-  <StuckOnInput />,
-  document.getElementById('stuckOnInput')
+});
+
+ReactDOM.render(
+    <StuckOnInput />,
+    document.getElementById('stuckOnInput')
 );
